c27645628261fb7be917c356f1aabfeb8599e906
Sylvestre22 <msylvestreets@yahoo.fr>
2024-05-24 15:28:37 +0200

    progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -6,14 +6,37 @@ import {
   IonContent,
   IonIcon,
 } from '@ionic/angular/standalone';
+import { UserService } from '../core/services/user.service';
+import { IUserTest } from '../core/interfaces/userTest';
+import { PhoneNumberPipe } from '../core/pipe/phone-number.pipe';
+import { LocalStorageService } from '../core/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonIcon,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    PhoneNumberPipe,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  users: IUserTest[] = [];
+  private userService = inject(UserService);
+  private local = inject(LocalStorageService);
+
+  ngOnInit() {
+    const users: IUserTest[] = this.local.getElement('users');
+    if (!users)
+      this.userService.requestTest().subscribe((response: IUserTest[]) => {
+        this.users = response;
+        this.local.setElement('users', JSON.stringify(this.users));
+      });
+    else this.users = users;
+  }
 }
